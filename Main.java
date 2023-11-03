12bca47f33ed3dc642aebe0b19a8415c605049f0
gabeewsuk <gabeewsuk@gmail.com>
2023-11-03 08:35:05 -0600

    sorting kinda working

--- Main.java
+++ Main.java
@@ -1,148 +1,142 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static int cycles = 0;
-    public static int arrivalTime = 0;
     public static int heapSize = 0;
+    public static Process[] processHeap; // Custom heap structure
 
     public static void main(String[] args) {
+        processHeap = new Process[100]; // Assuming a maximum of 100 processes
+        
         try {
             String filePath = "input.txt";
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
 
             String line;
-            Process[] processes = new Process[100];
             int processId = 0;
 
             while ((line = reader.readLine()) != null) {
                 processId++;
                 String[] numbers = line.split(" ");
-                arrivalTime = Integer.parseInt(numbers[0]);
+                int arrivalTime = Integer.parseInt(numbers[0]);
                 int runningTime = Integer.parseInt(numbers[1]);
                 int priority = Integer.parseInt(numbers[2]);
 
-                Process process = new Process(processId, priority, runningTime);
-                CompletableFuture<Void> processFuture = CompletableFuture.runAsync(() -> process.run());
-                line = reader.readLine();
-                if (line != null) {
-                    arrivalTime = Integer.parseInt(line.split(" ")[0]);
-                }
-
-                processes[heapSize] = process;
-                heapSize++;
-                if (cycles == 0) {
-                    heapifyUp(processes, heapSize - 1);
-                } else {
-                    Process minProcess = extractMin(processes, heapSize);
-                    CompletableFuture<Void> minProcessFuture = CompletableFuture.runAsync(() -> minProcess.run());
-                    minProcessFuture.get();
-                }
-
-                processFuture.get();
+                Process process = new Process(processId, priority, runningTime, arrivalTime);
+                addToHeap(process);
             }
 
             reader.close();
+        } catch (IOException e) {
+            System.err.println("An error occurred while reading the file: " + e.getMessage());
+        }
 
-            while (heapSize > 0) {
-                Process minProcess = extractMin(processes, heapSize);
-                heapSize--;
-                CompletableFuture<Void> processFuture = CompletableFuture.runAsync(() -> minProcess.run());
-                processFuture.get();
+        while (heapSize > 0) {
+            Process currentProcess = processHeap[0]; // Get the top process without removing it
+            boolean isCompleted = currentProcess.run();
+            if (isCompleted) {
+                removeTopProcess();
             }
-        } catch (IOException | InterruptedException | ExecutionException e) {
-            System.err.println("An error occurred: " + e.getMessage());
         }
     }
 
-    private static void heapifyUp(Process[] processes, int index) {
-        while (index > 0) {
-            int parentIndex = (index - 1) / 2;
-            if (processes[index].compareTo(processes[parentIndex]) < 0) {
-                swap(processes, index, parentIndex);
-                index = parentIndex;
-            } else {
+    public static void addToHeap(Process process) {
+        // Add the process to the heap based on your criteria (priority and arrival time)
+        processHeap[heapSize] = process;
+        int current = heapSize;
+        while (current > 0) {
+            int parent = (current - 1) / 2;
+            if (compareProcesses(process, processHeap[parent])) {
                 break;
             }
+            swap(current, parent);
+            current = parent;
+        }
+        heapSize++;
+    }
+
+    public static void removeTopProcess() {
+        // Remove the top process from the heap and resort
+        if (heapSize == 0) {
+            return; // Heap is empty
+        }
+        heapSize--;
+        processHeap[0] = processHeap[heapSize];
+        int current = 0;
+        while (true) {
+            int leftChild = 2 * current + 1;
+            int rightChild = 2 * current + 2;
+            int smallest = current;
+            if (leftChild < heapSize && compareProcesses(processHeap[leftChild], processHeap[smallest])) {
+                smallest = leftChild;
+            }
+            if (rightChild < heapSize && compareProcesses(processHeap[rightChild], processHeap[smallest])) {
+                smallest = rightChild;
+            }
+            if (smallest == current) {
+                break;
+            }
+            swap(current, smallest);
+            current = smallest;
         }
     }
 
-    private static Process extractMin(Process[] processes, int size) {
-        if (size <= 0) {
-            return null;
-        }
-
-        Process min = processes[0];
-        processes[0] = processes[size - 1];
-        heapifyDown(processes, size - 1, 0);
-        return min;
+    public static void swap(int index1, int index2) {
+        Process temp = processHeap[index1];
+        processHeap[index1] = processHeap[index2];
+        processHeap[index2] = temp;
     }
 
-    private static void heapifyDown(Process[] processes, int size, int index) {
-        int smallest = index;
-        int left = 2 * index + 1;
-        int right = 2 * index + 2;
-
-        if (left < size && processes[left].compareTo(processes[smallest]) < 0) {
-            smallest = left;
-        }
-
-        if (right < size && processes[right].compareTo(processes[smallest]) < 0) {
-            smallest = right;
-        }
-
-        if (smallest != index) {
-            swap(processes, index, smallest);
-            heapifyDown(processes, size, smallest);
-        }
+     public static boolean compareProcesses(Process p1, Process p2) {
+    if (p1.getPriority() < p2.getPriority() && p1.getArrivalTime() > p2.getArrivalTime()) {
+        return false;
+    } else if (p1.getPriority() == p2.getPriority() && p1.getArrivalTime() > p2.getArrivalTime()) {
+        return false;
     }
-
-    private static void swap(Process[] processes, int i, int j) {
-        Process temp = processes[i];
-        processes[i] = processes[j];
-        processes[j] = temp;
-    }
+    return true;
 }
 
-class Process implements Comparable<Process> {
+    
+}
+
+class Process {
     private int processId;
     private int priority;
     private int runningTime;
     private int cyclesRun;
+    private int arrivalTime;
 
-    public Process(int processId, int priority, int runningTime) {
+    public Process(int processId, int priority, int runningTime, int arrivalTime) {
         this.processId = processId;
         this.priority = priority;
         this.runningTime = runningTime;
         this.cyclesRun = 0;
+        this.arrivalTime = arrivalTime;
     }
 
-    public void run() {
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public boolean run() {
         Main.cycles++;
-        while (cyclesRun < runningTime) {
-            cyclesRun++;
-            Main.cycles++;
-            System.out.println("On cycle: " + Main.cycles + " the process " + processId + " ran for " + cyclesRun + " time(s)" + " priority " + priority);
-            //System.out.println("main.arrival " + Main.arrivalTime+ "   main.cycles "+Main.cycles);
-            if (Main.arrivalTime == Main.cycles) {
-                break;
-            }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        cyclesRun++;
+        System.out.println("On cycle: " + Main.cycles + " the process " + processId + " ran for " + cyclesRun + " time(s)" + " priority " + priority);
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        if (runningTime == cyclesRun) {
+        if (cyclesRun == runningTime) {
             System.out.println("Process ID: " + processId + " has completed.");
+            return true;
         }
-    }
-
-    @Override
-    public int compareTo(Process other) {
-        return Integer.compare(this.priority, other.priority);
+        return false;
     }
 }
